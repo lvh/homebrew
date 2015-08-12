de12644e3638e8c7a3de5b6ef421af6cac879ce4
Laurens Van Houtven <_@lvh.io>
2015-08-11 18:39:54 -0700

    Don't repeat version

--- Library/Formula/rack.rb
+++ Library/Formula/rack.rb
@@ -1,8 +1,8 @@
 class Rack < Formula
   desc "A CLI for Rackspace"
   homepage "https://github.com/rackspace/rack"
-  url "https://github.com/rackspace/rack/archive/1.0.0-beta.1.tar.gz"
   version "1.0.0-beta.1"
+  url "https://github.com/rackspace/rack/archive/#{version}.tar.gz"
   sha256 "2ab9513fee4a23667e89c05548be26b4f15a387a8be1e1896936f6e253e05c51"
 
   depends_on "go" => :build
